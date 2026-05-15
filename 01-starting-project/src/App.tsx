2adb51d794f1e5c45f10cb884045360d526cc05d
lawrencedovin <lawrencedovin@gmail.com>
2026-05-14 20:58:10 -0400

    Added core concepts section

--- 01-starting-project/src/App.tsx
+++ 01-starting-project/src/App.tsx
@@ -1,11 +1,18 @@
 import Header from "./Header.tsx";
+import CoreConcept from "./CoreConcept.tsx";
+import { CORE_CONCEPTS } from "./data.ts";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept) => <CoreConcept {...concept}/>)}
+          </ul>
+        </section>
       </main>
     </div>
   );
